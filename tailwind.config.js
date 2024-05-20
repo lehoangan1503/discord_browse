ef2f8152a46c5fdbfa7340311c79e722d63f16eb
An Le Hoang <lehoangan15031998@gmail.com>
2024-05-20 09:40:25 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{html,js}"],
+  theme: {
+    extend: {
+      animation: {
+        spotlight: "spotlight 2s ease 0.75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": { opacity: "0", transform: "translate(-72%, -62%) scale(.5)" },
+          "100%": { opacity: "1", transform: "translate(-50%, -40%) scale(1)" },
+        },
+      },
+    },
+    stroke: {
+      current: "currentColor",
+    },
+  },
+  plugins: [require("daisyui")],
+};
